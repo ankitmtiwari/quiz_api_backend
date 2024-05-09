85b64594dfed8f95632622fa071034c6548a1ccb
ankitmtiwari <tankit291389@gmail.com>
2024-05-09 22:13:03 +0530

    question  routes documented

--- src/routes/question_routes.js
+++ src/routes/question_routes.js
@@ -10,18 +10,27 @@ import { checkAuthMiddleware } from "../middlewares/auth_middleware.js";
 
 const questionRoutes = Router();
 
+//create question
 questionRoutes
   .route("/create")
   .post(checkAuthMiddleware, createQuestionController);
+
+//update question
 questionRoutes
   .route("/update")
   .post(checkAuthMiddleware, updateQuestionController);
+
+//delete question
 questionRoutes
   .route("/delete")
   .post(checkAuthMiddleware, deleteQuestionController);
+
+//get all questions
 questionRoutes
   .route("/getall")
   .post(checkAuthMiddleware, getAllQuestionController);
+
+//get any one random question based on the given parameters
 questionRoutes.route("/getRandomQuestion").get(getRandomQuestionController);
 
 export { questionRoutes };
